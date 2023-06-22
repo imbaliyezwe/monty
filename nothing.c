e48284e52172f935056525f3d66ee5d46f1fd625
Sakhile <fortunekhamz@gmail.com>
2023-06-22 01:06:47 -0700

    Monty

---
+++ nothing.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@head: head of the stack
+  *@counter: pointer to line number
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
