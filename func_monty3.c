2744c254e9e5a7d0dbb1ddd6ae13375fee5e73b7
Sakhile <fortunekhamz@gmail.com>
2023-06-24 06:30:04 -0700

    monty

---
+++ func_monty3.c
@@ -0,0 +1,58 @@
+#include "monty.h"
+
+void monty_nop(stack_t **stack, unsigned int line_number);
+void monty_pchar(stack_t **stack, unsigned int line_number);
+void monty_pstr(stack_t **stack, unsigned int line_number);
+
+/**
+ * monty_nop - Does nothing
+ * @stack: stack_t linked list.
+ * @line_number: line number of a Monty bytecodes file.
+ */
+void monty_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * monty_pchar - Prints the character in the top
+ * @stack: stack_t linked list.
+ * @line_number: line number of a Monty bytecodes file.
+ */
+void monty_pchar(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pchar_error(line_number, "stack empty"));
+		return;
+	}
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		set_op_tok_error(pchar_error(line_number,
+					     "value out of range"));
+		return;
+	}
+
+	printf("%c\n", (*stack)->next->n);
+}
+
+/**
+ * monty_pstr - Prints the string contained in a stack_t linked list.
+ * @stack: A pointer to the stack_t linked list.
+ * @line_number: line number of a Monty bytecodes file.
+ */
+void monty_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
+	{
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
+
+	printf("\n");
+
+	(void)line_number;
+}
